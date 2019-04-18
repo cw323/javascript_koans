299172c4d993c88874f113b1832b162649806343
Candy Wang <candywang013@gmail.com>
2019-04-18 11:27:17 -0700

    tests passing

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -2,30 +2,32 @@ describe("About Expects", function() {
 
   //We shall contemplate truth by testing reality, via spec expectations.  
   it("should expect true", function() {
-    expect(false).toBeTruthy(); //This should be true
+    expect(true).toBeTruthy(); //This should be true
   });
+
+  //exepctation: should be true, values that are false when truthy: falase, 0, "", null, undefined, and NaN
 
   //To understand reality, we must compare our expectations against reality.
   it("should expect equality", function () { 
-	  var expectedValue = FILL_ME_IN;
+	  var expectedValue = 2;
 	  var actualValue = 1 + 1;
 	
-	  expect(actualValue === expectedValue).toBeTruthy();
+	  expect(actualValue === expectedValue).toBeTruthy(); //expect 2 === 1+1 to be true
   });  
 
   //Some ways of asserting equality are better than others.
   it("should assert equality a better way", function () { 
-	  var expectedValue = FILL_ME_IN;
+	  var expectedValue = 2;
 	  var actualValue = 1 + 1;
 	
   // toEqual() compares using common sense equality.
-	  expect(actualValue).toEqual(expectedValue);
+	  expect(actualValue).toEqual(expectedValue); //expect 2 to equal 1+1
   });
 
   //Sometimes you need to be really exact about what you "type".
   it("should assert equality with ===", function () { 
-	  var expectedValue = FILL_ME_IN;
-	  var actualValue = (1 + 1).toString();
+	  var expectedValue = '2';
+	  var actualValue = (1 + 1).toString(); //this is changing the value to a string, expected value should be a string "2"
 	
   // toBe() will always use === to compare.
 	  expect(actualValue).toBe(expectedValue);
@@ -33,6 +35,6 @@ describe("About Expects", function() {
 
   //Sometimes we will ask you to fill in the values.
   it("should have filled in values", function () {
-	  expect(1 + 1).toEqual(FILL_ME_IN);
+	  expect(1 + 1).toEqual(2); //compares using common sense equality
   });
 });
